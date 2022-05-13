94ca7ff8da54c63aea310e270b8c77d048cbfb94
Jonathan Morales <jonmorales22@gmail.com>
2022-05-13 12:36:46 -0400

    creates login form component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css';
+import { Auth } from 'aws-amplify';
+import React, {Component} from "react";
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({signOut, user}) {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello {user.username}!
-          <button onClick={signOut}>Sign out</button>
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NameForm />
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault()
+    this.setState(
+      {
+        [event.target.name]: event.target.value
+      });
+  }
+
+  handleSubmit(event) {
+    alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="username"  value={this.state.username} onChange={this.handleChange} />
+        </label>
+        <br></br>
+        <label>
+          Password:
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+        <br></br>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default App
